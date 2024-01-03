79a3320380261f8e7cd1a5e40dd6c469abc65acd
Henrik Gyllensvaerd <henrik.gyllensvard@airtasker.com>
2024-01-03 17:00:15 +1100

    - Use jetpack compatible kotlin version. - Upgrade Jetpack compiler. - Use local package resolution or the javascript dependencies fail to download - Add the Platform.js.kt class. - Reference Okhttp3 in the Greeting class.

--- kotlinmultiplatformsharedmodule/src/commonMain/kotlin/au/airtasker/kotlinmultiplatformsharedmodule/Greeting.kt
+++ kotlinmultiplatformsharedmodule/src/commonMain/kotlin/au/airtasker/kotlinmultiplatformsharedmodule/Greeting.kt
@@ -1,9 +1,11 @@
 package au.airtasker.kotlinmultiplatformsharedmodule
 
+import okhttp3.OkHttp
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        return "Hello, ${platform.name} + ${OkHttp.VERSION}}!"
     }
 }
